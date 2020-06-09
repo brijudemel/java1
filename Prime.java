ab4147b8e71a851716b5f7bdbac7f636b7a1c91b
brijudemel <63197998+brijudemel@users.noreply.github.com>
2020-06-09 22:30:23 +0530

    Prime.java created

---
+++ Prime.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Prime
+{
+    int n;
+    Prime(int n)        //Constructor
+    {
+        this.n=n;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);    //Scanner object created.
+        int n;
+        while(true)
+        {
+            System.out.println("Enter the No. to be checked");
+            n=sc.nextInt();
+            Prime ob = new Prime(n);
+            if(ob.check())
+            {
+                System.out.println("It is a PRIME Number.");
+            }
+            else
+            {
+                System.out.println("It is not a PRIME Number.");
+            }
+            ob=null;
+            int choice;       //To check it more than once.
+            System.out.println("Enter 1 to check another number, else press any other key");
+            choice=sc.nextInt();
+            if(choice!=1)
+                break;
+        }
+    }
+    boolean check()       //Checks if the number is prime or not.
+    {
+        boolean b=true;
+        for(int i=2;i<n;i++)
+        {
+            if(n%i==0)
+            {
+                b=false;
+                break;
+            }
+        }
+        return b;       //Returns a boolean value.
+    }
+}
